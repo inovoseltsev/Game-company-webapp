24e27f3d34a93d7990d7110d1fd2e6914adcf4e9
Ilya <inovoseltsev>
2020-07-10 19:13:02 +0300

    Fixed login form processing bug

--- src/main/java/com/inovoseltsev/holloball/controller/LoginController.java
+++ src/main/java/com/inovoseltsev/holloball/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.inovoseltsev.holloball.controller;
 
 import com.inovoseltsev.holloball.message.ErrorMessage;
+import com.inovoseltsev.holloball.model.entity.AppUser;
 import com.inovoseltsev.holloball.model.service.AppUserService;
 import com.inovoseltsev.holloball.model.state.State;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -36,15 +38,11 @@ public class LoginController {
     @RequestMapping("/failed")
     public String setFailPage(@RequestParam Map<String, String> userLoginData,
                               Model model) {
-        if (userLoginData.containsKey("bannedGoogleAccount")) {
+        AppUser user = userService.findByLogin(userLoginData.get("login"));
+        if (user == null || user.getState().equals(State.ACTIVE)) {
+            model.addAttribute("errorMessage", ErrorMessage.getLoginError());
+        } else {
             model.addAttribute("errorMessage", ErrorMessage.getBannedError());
-        } else if (userLoginData.containsKey("login")) {
-            if (userService.findByLogin(userLoginData.get("login")).getState()
-                    .equals(State.BANNED)) {
-                model.addAttribute("errorMessage", ErrorMessage.getBannedError());
-            } else {
-                model.addAttribute("errorMessage", ErrorMessage.getLoginError());
-            }
         }
         return "sign-in";
     }
